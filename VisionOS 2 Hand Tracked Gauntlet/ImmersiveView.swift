e5ba7883820a53dadfc0f27e1065437cba206234
coledennis <36555688+coledennis@users.noreply.github.com>
2024-11-30 12:46:22 -0600

    Set up hand tracking and add gauntlet scene to be anchored to the user's hand.

--- VisionOS 2 Hand Tracked Gauntlet/ImmersiveView.swift
+++ VisionOS 2 Hand Tracked Gauntlet/ImmersiveView.swift
@@ -11,14 +11,30 @@ import RealityKitContent
 
 struct ImmersiveView: View {
 
+    @State private var session: SpatialTrackingSession?
+    
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
-            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(immersiveContentEntity)
+            
+            //Begin a spatial tracking session to understand the location of the user's hands.
 
-                // Put skybox here.  See example in World project available at
-                // https://developer.apple.com/
+            let session = SpatialTrackingSession()
+            let configuration = SpatialTrackingSession.Configuration(tracking: [.hand])
+            _ = await session.run(configuration)
+            self.session = session
+            
+            //Setup an anchor at the user's left palm.
+            let handAnchor = AnchorEntity(.hand(.left, location: .palm), trackingMode: .continuous)
+
+            //Add the Gauntlet scene that was set up in Reality Composer Pro.
+            if let gauntletEntity = try? await Entity(named: "Gauntlet", in: realityKitContentBundle) {
+               
+                //Child the gauntlet scene to the handAnchor.
+                handAnchor.addChild(gauntletEntity)
+                
+                // Add the handAnchor to the RealityView scene.
+                content.add(handAnchor)
+               
             }
         }
     }
