1a51840fdfaccb2e00446235e735fc594468b974
coledennis <36555688+coledennis@users.noreply.github.com>
2024-12-07 10:05:38 -0500

    Update the gauntet position and rotation, and update button text.

--- VisionOS 2 Hand Tracked Gauntlet/ToggleImmersiveSpaceButton.swift
+++ VisionOS 2 Hand Tracked Gauntlet/ToggleImmersiveSpaceButton.swift
@@ -49,7 +49,7 @@ struct ToggleImmersiveSpaceButton: View {
                 }
             }
         } label: {
-            Text(appModel.immersiveSpaceState == .open ? "Hide Immersive Space" : "Show Immersive Space")
+            Text(appModel.immersiveSpaceState == .open ? "Hide Immersive Space With Gauntlet" : "Show Immersive Space With Gauntlet")
         }
         .disabled(appModel.immersiveSpaceState == .inTransition)
         .animation(.none, value: 0)
